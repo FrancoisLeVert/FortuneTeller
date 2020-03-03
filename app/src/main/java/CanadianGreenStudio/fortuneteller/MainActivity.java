414445ce4d06c95c7a681b0889bc07e3eb871e61
Francois LeVert <fr644321@dal.ca>
2020-03-03 08:36:05 -0400

    Removing mistaken inner directory

--- app/src/main/java/CanadianGreenStudio/fortuneteller/MainActivity.java
+++ app/src/main/java/CanadianGreenStudio/fortuneteller/MainActivity.java
@@ -1,9 +1,5 @@
 package CanadianGreenStudio.fortuneteller;
 
-import android.app.DatePickerDialog;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,17 +11,8 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TextView;
-
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "Main Activity";
-    private TextView DisplayDate;
-    private DatePickerDialog.OnDateSetListener DateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,45 +21,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final String[] birthday = new String[1];
-
-        DisplayDate = findViewById(R.id.birthdayInput);
-
-        DisplayDate.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(
-                        MainActivity.this,
-                        android.R.style.Theme_DeviceDefault_Light_Dialog,
-                        DateSetListener,
-                        year, month, day);
-
-                dialog.show();
-            }
-        });
-
-        DateSetListener = new DatePickerDialog.OnDateSetListener() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                month = month + 1;
-                String date = month + "/" + dayOfMonth + "/" + year;
-                DisplayDate.setText(date);
-                birthday[0] = date;
-            }
-        };
-
-        Button submitButton = findViewById(R.id.birthdayButton);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(v.getContext(), Horoscope.class);
-                intent.putExtra("Birthday info", birthday[0]);
-                startActivity(intent);
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
